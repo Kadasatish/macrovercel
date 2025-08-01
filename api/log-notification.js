63f089b7eb2de4c0a732a3db4be380dbfd6d4a58
Kadasatish <kadasatish2022@gmail.com>
2025-08-01 19:16:59 +0530

    Update log-notification.js

--- api/log-notification.js
+++ api/log-notification.js
@@ -1,12 +1,8 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-app.all((req, res) => {
-  console.log('Received notification:', req.body);
-  res.status(200).json({ message: 'Notification received' });
-});
-
-module.exports = app;
-<<<api/log-notification.js 
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('Received notification:', req.body);
+    res.status(200).json({ message: 'Notification received' });
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
